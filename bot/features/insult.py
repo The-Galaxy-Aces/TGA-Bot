2c77c9ce04bca62ab4c4bacfa4070094f4978a9e
Travis Neely <travisneely@gmail.com>
2020-09-22 01:25:45 +0000

    Update to use Cogs

--- bot/features/insult.py
+++ bot/features/insult.py
@@ -1,18 +1,22 @@
 import requests
 import html
+from discord.ext import commands
 
 
-class Insult:
+class Insult(commands.Cog):
     """The Insult class
     Don't let the mean insults hurt your feelings
     """
-    def __init__(self):
-        """The Insult class
+    def __init__(self, bot):
+        """The Insult Cog
         Don't let the mean insults hurt your feelings
         """
-        #TODO: allow for a variety of insults from different APIs
-        #TODO: ADD: https://insult.mattbas.org/api
-        #TODO: ADD: https://generatorfun.com/insult-generator
+        self.bot = bot
+        self._last_member = None
+
+        # TODO: allow for a variety of insults from different APIs
+        # TODO: ADD: https://insult.mattbas.org/api
+        # TODO: ADD: https://generatorfun.com/insult-generator
 
         self.uri = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
         self.insult = ""
@@ -28,3 +32,21 @@ class Insult:
     def getInsult(self):
         self.generateInsult()
         return html.unescape(self.insult)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Insults are ready!")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        '''on_message left as an example for how to call it'''
+        return
+
+    @commands.command()
+    async def insult(self, ctx):
+        if ctx.message.author == self.bot.user:
+            return
+        else:
+            for mention in ctx.message.mentions:
+                await ctx.message.channel.send(mention.mention +
+                                               self.getInsult())
