402e7faa7808b567cdfaab9054a0889eb370e947
Rob D <rcdelga@outlook.com>
2020-09-25 19:23:24 -0500

    casing changes + command prefix reversion

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+import discord
 import discord.ext
 from time import localtime, strftime, sleep
 
@@ -14,7 +15,7 @@ class Bot(discord.ext.commands.Bot):
     """
 
     def __init__(self, config_file):
-        command_prefix = "."
+        command_prefix = "!"
         super().__init__(command_prefix)
 
         # Check for config file
@@ -23,19 +24,19 @@ class Bot(discord.ext.commands.Bot):
 
         # Read in config file
         with open(config_file, 'r') as config_json:
-            config = json.load(config_json)
+            CONFIG = json.load(config_json)
 
         # Check bot for minimal required params to make bot run properly
-        required_params = ['bot_name', 'token']
-        missing_params = [param for param in required_params if not config.get(param)]
-        if missing_params:
-            raise AssertionError(f"config.json missing {missing_params}")
+        REQUIRED_PARAMS = ['bot_name', 'token']
+        MISSING_PARAMS = [param for param in REQUIRED_PARAMS if not CONFIG.get(param)]
+        if MISSING_PARAMS:
+            raise AssertionError(f"config.json missing {MISSING_PARAMS}")
 
         # Pull information out of parsed config file
-        self.name = config.get('bot_name')
-        self.token = config.get('token')
-        self.enabled_features = config.get('enabled_features')
-        self.logging = config.get('logging')
+        self.name = CONFIG.get('bot_name')
+        self.token = CONFIG.get('token')
+        self.enabled_features = CONFIG.get('enabled_features')
+        self.logging = CONFIG.get('logging')
 
         # Logging setup
         if bool(self.logging['enabled']):
@@ -44,7 +45,7 @@ class Bot(discord.ext.commands.Bot):
             FILE_NAME = f"discordBot-{self.name}-{DATE_STAMP}.log"
 
             self.log = logging.getLogger(f"{self.name} Logger")
-            self.log.setLevel(config['logging']['logging_level'])
+            self.log.setLevel(CONFIG['logging']['logging_level'])
             self.handler = logging.FileHandler(filename=FILE_NAME)
             self.handler.setFormatter(logging.Formatter(FORMAT))
             self.log.addHandler(self.handler)
