46c40ecf1c84673fb401c2a49bbbf1704e0656c5
Rob D <rcdelga@outlook.com>
2020-09-25 19:00:09 -0500

    initial commit

--- bot/bot.py
+++ bot/bot.py
@@ -1,73 +1,66 @@
-import discord
-import discord.ext
-import logging
+import os
 import json
+import logging
+import discord.ext
 from time import localtime, strftime, sleep
 
 from bot.features.insult import Insult
 
 
 class Bot(discord.ext.commands.Bot):
-    """The Bot class.
+    """
+    The Bot class.
     The bot can do lots of neat things
     """
-    def __init__(self, name, configFile):
 
-        command_prefix = "!"
+    def __init__(self, config_file):
+        command_prefix = "."
         super().__init__(command_prefix)
 
-        self.name = ""
-        self.token = ""
-        self.configDict = {}
-        self.features = {}
+        # Check for config file
+        if not os.path.exists(config_file):
+            raise OSError(f"{config_file} not found or missing")
+
+        # Read in config file
+        with open(config_file, 'r') as config_json:
+            config = json.load(config_json)
+
+        # Check bot for minimal required params to make bot run properly
+        required_params = ['bot_name', 'token']
+        missing_params = [param for param in required_params if not config.get(param)]
+        if missing_params:
+            raise AssertionError(f"config.json missing {missing_params}")
+
+        # Pull information out of parsed config file
+        self.name = config.get('bot_name')
+        self.token = config.get('token')
+        self.enabled_features = config.get('enabled_features')
+        self.logging = config.get('logging')
 
         # Logging setup
-        self.log = logging.getLogger(name + 'Logger')
-        # TODO make the logging level configurable at init
-        self.log.setLevel(logging.DEBUG)
-        self.handler = logging.FileHandler(
-            filename='discordBot-' + name + "-" +
-            strftime("%Y-%m-%d", localtime()) + ".log")
-        self.handler.setFormatter(
-            logging.Formatter(
-                '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-        self.log.addHandler(self.handler)
+        if bool(self.logging['enabled']):
+            FORMAT = "%(asctime)s:%(levelname)s:%(name)s: %(message)s"
+            DATE_STAMP = strftime("%Y-%m-%d", localtime())
+            FILE_NAME = f"discordBot-{self.name}-{DATE_STAMP}.log"
 
-        self.process_config(configFile)
-        print("\nStarting up " + self.name + "!\n")
+            self.log = logging.getLogger(f"{self.name} Logger")
+            self.log.setLevel(config['logging']['logging_level'])
+            self.handler = logging.FileHandler(filename=FILE_NAME)
+            self.handler.setFormatter(logging.Formatter(FORMAT))
+            self.log.addHandler(self.handler)
 
-        self.enable_features()
+            self.log.info("Bot initalized")
 
+        # Features
+        self.listEnabledFeatures()
         self.add_cog(Insult(self))
-        self.log.info("Bot initalized")
 
-    def process_config(self, configFile):
-        with open(configFile, "r") as file:
-            self.configDict = json.load(file)
-        self.name = self.configDict["bot_name"]
-        self.token = self.configDict["token"]
-        if self.token == "Your Discord API Key" or not self.token:
-            print("ERROR: No API Key. Please configure in config.json")
-            self.log.error("No API Key. Please configure in config.json")
-
-    def enable_features(self):
-
+    def listEnabledFeatures(self):
         print("Enabled features:")
-        for x in self.configDict["enabled_features"]:
-            if self.configDict["enabled_features"][x]["enabled"] == "True":
-                self.features[x] = "True"
-                print(f'\t{x}')
-        print("")
-
-    def set_logging_level(self, level):
-        if level in [
-                logging.NOTSET, logging.DEBUG, logging.INFO, logging.WARNING,
-                logging.ERROR, logging.CRITICAL
-        ]:
-            self.log.setLevel(logging.DEBUG)
-        else:
-            self.log.error("Bot.set_logging_level: Invalid logging level: " +
-                           level)
+        for enabled_feature in self.enabled_features:
+            if bool(self.enabled_features[enabled_feature]["enabled"]):
+                print(f"{enabled_feature}")
+        print()
 
     def get_token(self):
         return self.token
