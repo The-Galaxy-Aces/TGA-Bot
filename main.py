9ed00bf6ff18c0779352c56b7c1deabf89edaaf7
Travisivart <Travisivart@users.noreply.github.com>
2020-10-01 04:16:41 -0500

    Major updates: Multiple bots with threading, json converted to yaml (#15)
    * Major updates: Multiple bots with threading, json converted to yaml
    
    Now supporting multiple bots. Each bot has a section in the config,
    when the config is read it will create and run a new bot in its own
    thread.
    
    command_prefix is now defined in the config.
    
    Converted the config.json to config.yaml.
    
    * Removed unneeded import yaml
    
    * Updated requirements to include pyyaml
    
    * Added newline to build.sh
    
    * Fixed incorrect bool usage
    
    * Update main.py
    
    Co-authored-by: Rob D. <43051972+rcdelga@users.noreply.github.com>
    
    * Fixed threading issues on windows
    
    Windows threw a weird NotImplementedError
    on the asyncio.get_child_watcher()
    
    * Trying again to fix the Windows threading in the previous commit
    
    Co-authored-by: Rob D. <43051972+rcdelga@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
+import os
+import threading
+import asyncio
+import yaml
+import sys
 from bot.bot import Bot
+
+
+async def threadedBot(bot):
+    await bot.start(bot.get_token())
+
+
+def loopTheBot(loop):
+    if not loop.is_running():
+        loop.run_forever()
+
 
 def main():
 
-    bot = Bot("config.json")
-    bot.run(bot.get_token())
+    config_file = "config.yaml"
+
+    # Check for config file
+    if not os.path.exists(config_file):
+        raise OSError(f"{config_file} not found or missing")
+
+    # Read in config file
+    with open(config_file, 'r') as config_yaml:
+        CONFIG = yaml.full_load(config_yaml)
+
+    for botConfig in CONFIG["bots"]:
+        if sys.version_info[0] == 3 and sys.version_info[
+                1] >= 8 and sys.platform.startswith('win'):
+            asyncio.set_event_loop_policy(
+                asyncio.WindowsSelectorEventLoopPolicy())
+        else:
+            asyncio.get_child_watcher()
+
+        loop = asyncio.get_event_loop()
+
+        bot = Bot(botConfig["config"])
+        loop.create_task(threadedBot(bot))
+
+        thread = threading.Thread(target=loopTheBot, args=(loop, ))
+        thread.start()
 
 
 if __name__ == "__main__":
