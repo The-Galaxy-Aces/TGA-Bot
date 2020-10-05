b26dfa6d820598a324854e64fbfaeb3f64e7c811
Travis Neely <travisneely@gmail.com>
2020-10-05 16:54:07 +0000

    Update paths for the insult feature

---
+++ bot/features/TGABot.py
@@ -0,0 +1,11 @@
+import discord
+import discord.ext
+from discord.ext import commands
+
+
+class TGABot(commands.Cog):
+    def __init__(self, CONFIG):
+        REQUIRED_PARAMS = []
+
+    def processConfig(CONFIG):
+        pass
