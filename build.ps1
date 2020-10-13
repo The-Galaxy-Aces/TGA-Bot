4781852724e4800d96d41c3d2e8f561735ca7615
Rob D <rcdelga@outlook.com>
2020-10-13 15:32:37 -0500

    PowerShell now checks for config.yaml.

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ Write-Information "Starting build.ps1..."
 Write-Information "Checking for venv in $(Get-Location)"
 IF (-NOT (Test-Path -Path ".\venv")) {
     Write-Information "Not Found: Creating new venv!"
-    python -m venv venv    
+    python -m venv venv
 }
 
 Write-Information "Starting up venv..."
@@ -14,6 +14,11 @@ python -m pip install --upgrade pip
 IF (Test-Path -Path ".\requirements.txt") {
     Write-Information "Installing packages from requirements.txt..."
     pip install -r requirements.txt
+}
+
+IF (-NOT (Test-Path "config.yaml")) {
+    Write-Information "File not found: config.yaml. Now creating default config file..."
+    Get-Content defaults.yaml > config.yaml
 }
 
 Write-Information "Closing down venv..."
